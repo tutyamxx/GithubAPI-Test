5a1e3539296d05121655eb5fa87cdcbd83e0a8a3
tutyamxx <tutygoo@gmail.com>
2020-02-01 15:53:54 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // --| Small express server to run our Vue.JS app on Heroku
-import express from "express";
-import serveStatic from "serve-static";
-import { get } from "http";
+const express = require("express");
+const http = require("http");
+const serveStatic = require("serve-static");
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.listen(port);
 // --| Ping Heroku app and prevent it from sleeping every 15 minutes
 setInterval(() =>
 {
-    get("http://mabletest.herokuapp.com/");
+    http.get("http://mabletest.herokuapp.com/");
 
 }, 900000);
 
