8b157f238643409ae9dea2eb41cb55855231cf69
tutyamxx <tutygoo@gmail.com>
2020-02-03 00:21:19 +0000

    Updates

--- tests/unit/SearchUsers.spec.js
+++ tests/unit/SearchUsers.spec.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-console */
 import { expect } from "chai";
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import SearchUsers from "@/components/SearchUsers.vue";
 
 describe("Some Simple Testing", () =>
@@ -8,7 +9,7 @@ describe("Some Simple Testing", () =>
     
     beforeEach(() =>
     {
-        wrapper = shallowMount(SearchUsers);
+        wrapper = mount(SearchUsers);
     });
 
     it("Should render the search box", () =>
@@ -43,4 +44,25 @@ describe("Some Simple Testing", () =>
         SearchButton.trigger("click");
         expect(ErrorMessageElement.text()).to.be.equal("Search field cannot be empty!").and.not.to.be.empty;
     });
+
+    it("Should load the animation upon search", () =>
+    {
+        const SearchButton = wrapper.find("button");
+
+        expect(SearchButton.attributes("id")).to.be.equal("search-user");
+        expect(SearchButton).to.exist.to.be.a("object");
+        expect(Object.keys(SearchButton).length).to.be.equal(0);
+        expect(SearchButton.text()).to.be.equal("Search");
+
+        const SearchBox = wrapper.find('input[type="text"]');
+        expect(SearchBox.attributes("class")).to.be.equal("search");
+        SearchBox.setValue("testing loading");
+
+        SearchButton.trigger("click");
+
+        const LoadingAnimationDiv = wrapper.find(".loading");
+        expect(LoadingAnimationDiv.isVisible()).to.be.true;
+    });
+
+    afterEach(() => { wrapper.destroy() });
 });
